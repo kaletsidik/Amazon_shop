4c1713db0ae8d62681e090b2fe180500848b97fa
kaletsidik <awokeworket@gmail.com>
2025-01-03 19:59:24 +0300

    update axios

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-     baseURL:"http://127.0.0.1:5001/clone-3f208/us-central1/app"
-})
+  // baseURL:"http://127.0.0.1:5001/clone-3f208/us-central1/app"
+  baseURL: "https://amazon-api-deploy-uc8c.onrender.com/",
+});
 export { axiosInstance };
